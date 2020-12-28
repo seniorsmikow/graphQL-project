e0da127f1a31a0082f1b3cebc4a268753e015c0c
seniorsmikow <senior.smikow@yandex.ru>
2020-12-28 19:17:28 +0300

    connected to mongoDB, change app.js

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
@@ -11,3 +11,19 @@ const MovieType = new GraphQLObjectType({
     })
 });
 
+const Query = new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        movie: {
+            type: MovieType,
+            args: {id: {type: GraphQLString}},
+            resolve: (parent, args) => {
+    
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: Query
+});
--- server/app.js
+++ server/app.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const schema = require('../schema/schema');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
 const PORT = 3005;
 
-app.use('/graphql', graphqlHTTP({}));
+mongoose.connect('mongodb+srv://Ilya:k1k4k88hail@cluster0.lfpic.mongodb.net/test', { useUnifiedTopology: true });
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+const dbConnection = mongoose.connection;
+
+dbConnection.on('error', error => console.log(`Connection error: ${error}`));
+
+dbConnection.once('open', () => console.log("Connected to db!"));
 
 app.listen(PORT, error => {
     error ? console.log(error) : console.log("Server started!");
 });
-
-
-// const express = require('express');
-// const graphqlHTTP = require('express-graphql');
-
-// const app = express();
-// const PORT = 3005;
-
-// app.use('/graphql', graphqlHTTP({}));
-
-// app.listen(PORT, err => {
-//   err ? console.log(err) : console.log('Server started!');
-// });
